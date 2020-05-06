39fe60b05ebc2cef3ae532dab41949e4cbb9b9c9
Vanessa G <61889874+vxnessa@users.noreply.github.com>
2020-05-05 23:11:01 -0500

    css redone

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,6 +2,7 @@ var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 
+
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
@@ -10,7 +11,7 @@ var playerMoney = 10;
 var fight = function(enemyName) {
     if (playerHealth > 0) {
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    }
+    };
 
 
     // fight function statements
@@ -26,7 +27,52 @@ if (promptFight === "skip" || promptFight === "SKIP") {
     //if yes (true), leave fight
     if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        // subtract money from playerMoney for skipping 
+        
+// Operator function
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth = enemyHealth - playerAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+
+// check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+} 
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+
+
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
+playerHealth = playerHealth - enemyAttack;
+
+// Log a resulting message to the console so we know that it worked.
+console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+);
+
+
+// check player's health
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} 
+else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
+
+
+
+
+
+
+
+
+// subtract money from playerMoney for skipping 
         playerMoney = playerMoney - 10;
         console.log("playerMoney", playerMoney);
       break;
@@ -78,5 +124,8 @@ break;
         
       
   }
+
+  //execute function
+  fight();
 
 
