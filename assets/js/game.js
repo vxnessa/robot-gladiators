0ade2ea8d95406df4dc8c0c4fb5007d6cb0a442c
Vanessa G <61889874+vxnessa@users.noreply.github.com>
2020-05-18 14:22:21 -0500

    debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,8 @@ var startGame = function() {
 
   for (var i = 0; i < enemyInfo.length; i++) {
     if (playerInfo.health > 0) {
-      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+      window.alert("Welcome to Battlebots! Round " + (i + 1));
+    
       var pickedEnemyObj = enemyInfo[i];
       pickedEnemyObj.health = randomNumber(40, 60);
       fight(pickedEnemyObj); 
@@ -201,8 +202,18 @@ enemy.health = Math.max(0, enemy.health - damage);
           }
       }
     };
+
+    var getPlayerName = function() {
+      var name = "";
+    
+      while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+  return name;
+    };
     var playerInfo = {
-      name: window.prompt("What is your robot's name?"),
+      name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
